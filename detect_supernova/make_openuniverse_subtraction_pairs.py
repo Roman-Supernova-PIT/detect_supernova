994014af8942c09035bdb18dadd92c0c132a41ae
Michael Wood-Vasey <wmwv@pitt.edu>
2025-07-09 17:29:00 -0400

    Rename find_template.  Make "astrophysical" and "ranmag" image pair test lists.

--- detect_supernova/make_openuniverse_subtraction_pairs.py
+++ detect_supernova/make_openuniverse_subtraction_pairs.py
@@ -1,5 +1,5 @@
 """
-Find templates for given images
+Find science, templates pairs for example RA, Dec for OpenUniverse2024 images
 """
 
 import requests
@@ -43,12 +43,7 @@ def get_templates_for_points(points, band, min_points=3):
     matches = pd.concat(matches)
     # From
     #  https://stackoverflow.com/questions/35584085/how-to-count-duplicate-rows-in-pandas-dataframe
-    matches = (
-        matches.groupby(matches.columns.tolist())
-        .size()
-        .reset_index()
-        .rename(columns={0: "counts"})
-    )
+    matches = matches.groupby(matches.columns.tolist()).size().reset_index().rename(columns={0: "counts"})
     good_matches = matches.loc[matches.counts >= min_points]
 
     return good_matches
@@ -87,27 +82,48 @@ def get_templates_for_image(im, min_points=3):
     return get_templates_for_points(points, band=band, min_points=min_points)
 
 
-def run():
+def run(
+    band="R062",
+    transient_type="astrophysical",
+    ra=[8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0],
+    dec=[-43.2, -43.2, -43.2, -43.2, -43.2, -43.1, -43.1, -43.1, -43.1, -43.1],
+):
     """Get templates for 10 different pointings
 
-    I don't know how to easily query the roman-desc-simdex for a given pointing, sca
+    I don't know how to query the roman-desc-simdex for a given pointing, sca
 
-    So I'm just going to do the silly thing and ask for 10 different RA, DEC positions
-    and use that image info.
+    So I'm just going to do the silly thing and ask for
+    10 different RA, DEC positions and use that image info.
+
+    From
+    https://arxiv.org/pdf/2501.05632
+    The RomanWAS MJD range is 61444 -- 63270
+    The RomanTDS MJD range is 62000 -- 63563
+
+    "astrophysical" are simulated based on 10 different types of astrophysical transients
+    from 61444, 63269.
+    The peak MJD range was from 61374 to 63299 so that the simulated range is complete
+
+    "ranmag" is a flat AB spectrum -22 < mag < -17 from 0 < z < 2.
+    meant for easy testing of subtractions
+    The ranmag are just the last 14 days of the simulated range.
     """
-    ra = [8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0]
-    dec = [-43.2, -43.2, -43.2, -43.2, -43.2, -43.1, -43.1, -43.1, -43.1, -43.1]
-    band = "R062"
+    mjd_range = {"astrophysical": (61444, 63269), "ranmag": (63549, 63563)}
+    mjd_min, mjd_max = mjd_range[transient_type]
 
     science_images = []
     for r, d in zip(ra, dec):
         image_info = get_image_info_for_ra_dec(r, d, band=band)
-        # Take the last one by MJD
-        science_image = image_info.iloc[image_info.mjd.argsort()].iloc[0]
+        # Take the last one by MJD that's within the MJD transient range
+        # of the OpenUniverse sims
+        image_info = image_info.loc[(mjd_min < image_info.mjd) & (image_info.mjd < mjd_max)]
+        science_image = image_info.iloc[image_info.mjd.argsort()].iloc[-1]
         science_images.append(science_image)
 
+    print(science_images)
     # Now we have 10 images with the information we need to get center and corners.
-    # Now we're going to We're just going to pick one for each and have our list.
+    # Now we're going to pick the earliest template for each science image
+    #  that overlaps at least 3 (corners, center).
     template_images = []
     for im in science_images:
         possible_templates = get_templates_for_image(im)
