802a32ebd8f278842d73e1a8c8d43b32f8a6ea72
Michael Wood-Vasey <wmwv@pitt.edu>
2025-07-25 19:23:36 -0400

    Add ability to reject entries from catalog that matches another.

--- sidecar/truth_matching.py
+++ sidecar/truth_matching.py
@@ -1,24 +1,43 @@
 import astropy.units as u
 
-from coord_projection import two_direction_skymatch
+from sidecar.coord_projection import one_direction_skymatch, two_direction_skymatch
 
-MATCH_RADIUS = 0.4
+MATCH_RADIUS = 0.4 * u.arcsec
 
 
-def skymatch_and_join(
-    left_table, right_table, left_skycoord, right_skycoord, match_radius=None
-):
-    match_radius = match_radius or MATCH_RADIUS
-
+def skymatch_and_join(left_table, right_table, left_skycoord, right_skycoord, match_radius=MATCH_RADIUS):
     left_table = left_table.copy().reset_index(drop=True)
     right_table = right_table.copy().reset_index(drop=True)
 
-    matched_status, matched_id = two_direction_skymatch(
-        left_skycoord, right_skycoord, radius=match_radius * u.arcsec
-    )
+    matched_status, matched_id = two_direction_skymatch(left_skycoord, right_skycoord, radius=match_radius)
     right_table = right_table.iloc[matched_id].copy().reset_index(drop=True)
 
     left_table["matched_status"] = matched_status
     joined_table = left_table.merge(right_table, left_index=True, right_index=True)
 
     return joined_table
+
+
+def skymatch_and_reject(left_table, right_table, left_skycoord, right_skycoord, match_radius=MATCH_RADIUS):
+    """Reject entries in 'left_table' that are within 'radius' of entries in 'right_table'.
+
+    Parameters
+    ----------
+    left_table : pandas.DataFrame
+    right_table : pandas.DataFrame
+    left_skycoord : AstroPy.coord.SkyCoord
+    right_skycoord : AstroPy.coord.SkyCCoord
+    radius: Quantity -> degree
+
+    Returns
+    -------
+    pandas.DataFrame
+        Entries in left_table that are not within radius of objects in right_table
+    """
+    left_table = left_table.copy().reset_index(drop=True)
+    right_table = right_table.copy().reset_index(drop=True)
+
+    matched_status, matched_id = one_direction_skymatch(left_skycoord, right_skycoord, radius=match_radius)
+    left_table = left_table.iloc[~matched_status]
+
+    return left_table
